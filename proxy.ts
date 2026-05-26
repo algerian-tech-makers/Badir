47376736f09356e27329626b6c34ec291823487a
MohamedMouloudj <mouloudy656565@gmail.com>
2026-05-27 00:16:17 +0100

    fix: added pdf extension to proxy for tems to be publicly visible

--- proxy.ts
+++ proxy.ts
@@ -59,6 +59,6 @@ export default async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|sitemap)).*)",
+    "/((?!_next|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|pdf|zip|webmanifest|sitemap)).*)",
   ],
 };
